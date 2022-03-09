564f8999bc48d10d8947f45af598db7208bd2a8d
Pasha-Akito <1masterpasha@gmail.com>
2022-03-09 01:17:01 +0000

    Added Profile and interests sidebar UI

---
+++ app/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar } from "@material-ui/core";
+
+const recentItem = (topic) => (
+  <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+  </div>
+) 
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+     
+      <div className='sidebar__top'>
+            <img
+                src="/gmitlogo.jpg"
+                alt='gmit logo'
+            />
+        <div className="sidebar__avatar">
+          <Avatar />
+        </div>
+        <h2>Your Name</h2>
+        <h4>Example@Email.com</h4>
+      </div>
+
+      <div className='sidebar__stats'>
+        
+        <div className="sidebar__stat">
+          <p>Who viewed your profile</p>
+          <p className="sidebar__statNumber">300</p>
+        </div>
+
+        <div className="sidebar__stat">
+          <p>Views of your posts</p>
+          <p className="sidebar__statNumber">5999</p>
+        </div>
+
+      </div>
+
+      <div className='sidebar_bottom'>
+      
+      <p>Recent</p>
+        {recentItem('Business')}
+        {recentItem('Basketball')}
+        {recentItem('Chess')}
+        {recentItem('Software Development')}
+        <h4>Followed Interests</h4>
+        {recentItem('Chess')}
+        {recentItem('Football')}
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
