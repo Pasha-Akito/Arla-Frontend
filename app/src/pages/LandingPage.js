01b018aeb75b130503a167532fdd96094ef0919c
Pasha-Akito <1masterpasha@gmail.com>
2022-03-29 14:52:11 +0100

    Profile Page being worked on, image working

--- app/src/pages/LandingPage.js
+++ app/src/pages/LandingPage.js
@@ -10,7 +10,7 @@ import Typography from '@mui/material/Typography';
 import { useAuth0 } from '@auth0/auth0-react'
 
 
-export default function SignInSide() {
+export default function LandingPage() {
 
     const { loginWithRedirect } = useAuth0()
 
--- app/src/pages/ProfilePage.js
+++ app/src/pages/ProfilePage.js
@@ -1,26 +1,123 @@
 import React from 'react'
 import './ProfilePage.css'
 import { useAuth0 } from '@auth0/auth0-react'
-import { Avatar} from '@material-ui/core'
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
 
-const Profile = () => {
+function ProfilePage() {
   const { user, isAuthenticated } = useAuth0()
 
   return (
-    isAuthenticated && (
-      <div>
-        <div className='profile'>
 
-          <div className='profile__top'>
-            <Avatar src={user.picture} className='profile__avatar' />
+    <Grid container sx={{
+      maxWidth: '1200px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      display: 'block'
+    }} >
+      <CssBaseline />
 
-            <h4>{user.name}</h4>
+      <Card sx={{
+        marginTop: '120px'
+      }}>
 
+        <div style={{
+          paddingRight: '24px',
+          paddingLeft: '24px'
+        }}>
+
+          <Grid item
+            sx={{
+              justifyContent: 'center',
+            }}>
+
+            <Grid item
+              lg={10}
+            >
+              <div className="profile__top">
+                <Avatar
+                  src={user.picture}
+                />
+              </div>
+            </Grid>
+
+            <Grid item
+              xs={12}
+            >
+
+              <div>
+                <div>
+                  <Button
+                  >
+                    Connect
+                  </Button>
+                  <Button
+                  >
+                    Message
+                  </Button>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <div>
+                    <span >22</span>
+                    <span >Friends</span>
+                  </div>
+                  <div>
+                    <span >10</span>
+                    <span >Photos</span>
+                  </div>
+                  <div>
+                    <span >89</span>
+                    <span >Comments</span>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+          <div>
+            <h3>
+              Jessica Jones{" "}
+              <span>, 27</span>
+            </h3>
+            <div>
+              <i />
+              Bucharest, Romania
+            </div>
+            <div>
+              <i />
+              Solution Manager - Creative Tim Officer
+            </div>
+            <div>
+              <i />
+              University of Computer Science
+            </div>
+          </div>
+          <div >
+            <div>
+              <div>
+                <p>
+                  An artist of considerable range, Ryan — the name taken
+                  by Melbourne-raised, Brooklyn-based Nick Murphy —
+                  writes, performs and records all of his own music,
+                  giving it a warm, intimate feel with a solid groove
+                  structure. An artist of considerable range.
+                </p>
+                <h4>Show More</h4>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    )
+
+      </Card>
+
+    </Grid>
   )
+
 }
 
-export default Profile
+export default ProfilePage
